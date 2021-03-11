58bc97fe893ffb1fcaa676bf5b545cbba2622fb3
bcopstein <bcopstein@gmail.com>
2021-03-11 22:38:41 +0000

    tudo bem

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Alo mundo!");
+        System.out.println("Tudo bem com você?");
     }
 }
